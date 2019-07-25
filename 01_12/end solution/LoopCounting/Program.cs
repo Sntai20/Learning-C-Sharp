a9b94dff4ea4a636131301faee583b7f02c1dc30
Antonio Santana <antonio.santana.ir@outlook.com>
2019-07-25 07:28:02 -0400

    Completed LoopCounting.

--- 01_12/end solution/LoopCounting/Program.cs
+++ 01_12/end solution/LoopCounting/Program.cs
@@ -6,12 +6,16 @@ namespace LoopCounting
     {
         static void Main(string[] args)
         {
+            // To count up to 10 and back to 1 five times, use this for loop.
             for (int x = 1; x <= 5; x++)
             {
+                // To count up to 10, use this for loop.
                 for (int i = 1; i <= 10; i++)
                 {
                     Console.WriteLine(i);
                 }
+
+                // To count down from 10 to 1, use this for loop.
                 for (int i = 10; i >= 1; i--)
                 {
                     Console.WriteLine(i);
